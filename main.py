ee9c951639794ed2a26661f47c8f4370ff0c8e17
BartoszPliszka <bartekpliszka@op.pl>
2026-04-08 14:55:28 +0200

    Initial model for AAPPL stock

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.features import add_technical_indicators, create_target
 from src.processor import DataProcessor
 import os
 import pandas as pd
+from src.model import StockModel
+
 
 TICKER = "AAPL"
 DATA_PATH = "data/processed_aapl.csv"
@@ -18,14 +20,23 @@ if __name__ == "__main__":
         df_close = raw_data.copy()
 
 
-    df_close = raw_data['AAPL'] if isinstance(raw_data, pd.DataFrame) else raw_data
-    df_with_features = add_technical_indicators(df_close)
-    df_with_features['target'] = create_target(df_close)
-    df_with_features = df_with_features.dropna()
+
+    df_final = add_technical_indicators(df_close)
+    df_final['target'] = create_target(df_close)
+    df_final = df_final.dropna()
 
     
     processor = DataProcessor()
-    X, y = processor.prepare_data(df_with_features)
+    X, y = processor.prepare_data(df_final)
     X_train, X_test, y_train, y_test = processor.split_data(X, y)
 
-    print(X_train)
+
+    model = StockModel()
+    print("TRAINING MODEL...")
+    model.train(X_train, y_train)
+
+    metrics = model.evaluate(X_test, y_test)
+    print(f"Model metrics: {metrics}")
+
+    predictions = model.predict(X_test)
+    print(f"First 5 forecasts: {predictions[:5]}")
